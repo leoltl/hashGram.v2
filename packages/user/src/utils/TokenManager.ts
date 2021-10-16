aa4f5e1478120cc2eae0f62e0c22cef1a31f9e90
Leo Lee <tl.leolee@gmail.com>
2021-10-15 21:14:19 -0400

    fix: token payload

--- packages/user/src/utils/TokenManager.ts
+++ packages/user/src/utils/TokenManager.ts
@@ -1,12 +1,18 @@
 import jwt from "jsonwebtoken";
-
-class TokenManager {
+import { User as GqlUserType } from "../resolvers-types";
+export class TokenManager {
   private secretKey = process.env.JWT_SECRET || "cannotGuessThis";
 
-  sign<T extends object>(data: T) {
-    return jwt.sign(data, this.secretKey);
+  sign<T extends GqlUserType>(user: T) {
+    return jwt.sign(
+      { name: user.name, email: user.email },
+      this.secretKey,
+      { 
+        algorithm: "HS256",
+        expiresIn: '10h',
+        issuer: 'hashGram.v2',
+        subject: user.id,
+      },
+    );
   }
-  
-}
-
-export default TokenManager;
+};
